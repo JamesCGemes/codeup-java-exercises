8fc5be4deb46cd69fbb34449704f60d8b79cba65
James Gemes <james.gemes@gmail.com>
2020-12-02 10:45:16 -0600

    finished the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,16 +2,29 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.print("Hello");
         System.out.print(" World ");
-
+//      1.
         int myFavoriteNumber = 33;
         System.out.println(myFavoriteNumber);
-
+//      2.prints the string
+//      3.When String is changed to char you get an error.
+//      4.If you put a number into the string using double quotes it prints the string
+//        If you don't use double quotes it shows an error
         String myString = "3.14159";
         System.out.println(myString);
+//      5. If you declare a varible but do not assign anything to it you get a might not have been initialized error
+//        Long myNumber;
 
-        Double myNumber = 3.14;
-        System.out.println(myNumber);
-
+//        6. If you assign a double to a Long,double cannot be converted to java.lang.Long
+//            Long myNumber = 3.14;
+//        7. prints 123
+//        Long myNumber = 123L;
+//        8.incompatible types: int cannot be converted to java.lang.Long
+//        Long myNumber = 123;
+//        9.incompatible types: possible lossy conversion from double to float.Needs an F at the end of the number. Or use Double data type.
+//        float myNumber = 3.14F;
+//        Double myNumber = 3.14;
+//        System.out.println(myNumber);
+//        10.
 //        int x = 5;
 //        Prints 5 then increments by one
 //        System.out.println(x++);
@@ -22,18 +35,29 @@ public class HelloWorld {
 //        Prints 6, x was pre-incremented
 //        System.out.println(++x);
 //        System.out.println(x);
-
+//        11.
 //        String class ="a string";
 //        Shows an error "not a statment"
-
+//        12.
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
 //        int three = (int) "three";
 ////        String cannot be converted to int
 
 //        13.
-        int x = 4;
-        x += 5;
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+//        14. If you assign a value to a numerical variable it will overflow, same if you increment past what the variable can hold.
 
     }
 }
