d4ae8a745e604752d3ead53bd6e5638b8fc11ea9
James Gemes <james.gemes@gmail.com>
2020-12-02 06:56:25 -0600

    working on java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.print("Hello");
+        System.out.print(" World ");
+
+        int myFavoriteNumber = 33;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        Double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        Prints 5 then increments by one
+//        System.out.println(x++);
+//        Prints 6
+//        System.out.println(x);
+
+//        int x = 5;
+//        Prints 6, x was pre-incremented
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class ="a string";
+//        Shows an error "not a statment"
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+////        String cannot be converted to int
+
+//        13.
+        int x = 4;
+        x += 5;
+
     }
 }
+
