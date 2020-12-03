b596134438089ae12e78333010a250ed84b60e73
James Gemes <james.gemes@gmail.com>
2020-12-03 17:02:13 -0600

    working  on table exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -59,19 +59,22 @@ public class ControlFlowExercises {
 //
 //        }
 
-// 3. FizzBuzz
-        for(int i = 1; i <= 100 ; i++){
-            if(((i % 3) == 0) && ((i % 5) == 0)){
-                System.out.println("Fizz");
-            }else if(i % 3 == 0){
-                System.out.println("Buzz");
-            }else if(i % 5 == 0){
-                System.out.println("FizzBuzz");
-            }else{
-                System.out.println(i);
-            }
+// 2. FizzBuzz
+//        for(int i = 1; i <= 100 ; i++){
+//            if(((i % 3) == 0) && ((i % 5) == 0)){
+//                System.out.println("FizzBuzz");
+//            }else if(i % 3 == 0){
+//                System.out.println("Fizz");
+//            }else if(i % 5 == 0){
+//                System.out.println("Buzz");
+//            }else{
+//                System.out.println(i);
+//            }
+//        }
 
-        }
+//        3.Display table of powers
+
+
 
 
 
