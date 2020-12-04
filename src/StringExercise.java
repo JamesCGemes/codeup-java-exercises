9cb22444a95e4b72bca45aee6fc5596ad0a25141
James Gemes <james.gemes@gmail.com>
2020-12-04 06:19:27 -0600

    finished string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+    public static void main(String[] args){
+
+       String strOne= "We don't need no education";
+       String strTwo = "We don't need no thought control";
+
+       System.out.println(strOne);
+       System.out.println(strTwo);
+
+        String strThree = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(strThree);
+
+        String strFour ="I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(strFour);
+
+    }
+}
