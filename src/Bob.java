97f384e9eb4a3d49e9bd7e1ef68c9463c75fc88b
James Gemes <james.gemes@gmail.com>
2020-12-04 12:14:23 -0600

    Removed unneeded comments

--- src/Bob.java
+++ src/Bob.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args){
 
-//        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-//        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-//        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
-//        He answers 'Whatever.' to anything else.
             Scanner sc = new Scanner(System.in);
 
             String sure = "Sure";
@@ -14,8 +10,7 @@ public class Bob {
             String fine = "Fine. Be that way";
             String whatever = "Whatever";
             int continueRunning = 1;
-//            String askAgain = "y";
-//            String stopAsking = "n";
+
 
             while(continueRunning != 0) {
             System.out.println("Say something to Bob!");
@@ -36,7 +31,7 @@ public class Bob {
 
             }
             System.exit(0);
-//            if answer  is y then
+
 
 
 
