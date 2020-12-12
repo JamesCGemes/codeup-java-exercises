879f2d992843c4a2e347bd75a60f087848d1b8af
James Gemes <james.gemes@gmail.com>
2020-12-12 17:50:33 -0600

    Arrays

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -8,6 +8,16 @@ public class ArraysExercises {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
 
+        Person[] people = new Person[3];
+
+        people[0] = new Person("James");
+        people[1] = new Person("Cassie");
+        people[2] = new Person("Jacob");
+
+        for(Person personsName : people){
+            System.out.println("Person name is " + personsName.getName());
+        }
+
 
 
 
