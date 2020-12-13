6ff8536907d69cd3feae1892792cf963b62bd6d2
James Gemes <james.gemes@gmail.com>
2020-12-13 13:40:15 -0600

    Finished person exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -14,12 +14,23 @@ public class ArraysExercises {
         people[1] = new Person("Cassie");
         people[2] = new Person("Jacob");
 
-        for(Person personsName : people){
-            System.out.println("Person name is " + personsName.getName());
+        for(Person person : people){
+            System.out.println(person.getName());
         }
 
+        Person[] newArray = addPerson(people, new Person("Lee"));
 
-
+        for(Person person: newArray){
+            System.out.println(person.getName());
+        }
 
     }
+
+    public static Person[] addPerson(Person[] peopleArray, Person personToAdd){
+        Person[] tempArray = Arrays.copyOf(peopleArray,peopleArray.length + 1);
+        tempArray[tempArray.length -1] = personToAdd;
+
+        return tempArray;
+    }
+
 }
