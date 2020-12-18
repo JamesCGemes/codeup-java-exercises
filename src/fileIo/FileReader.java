823feaa06870b079e0e8521b3a2d4fdbe7f14f6d
James Gemes <james.gemes@gmail.com>
2020-12-18 14:55:49 -0600

    IO lecture example

--- src/fileIo/FileReader.java
+++ src/fileIo/FileReader.java
@@ -1,60 +1,109 @@
 package fileIo;
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileReader {
 
-    private String directoryName; // data
-    private String fileName;
-    private String logFileName;  // store what went right or wrong
-    private Path directoryPath;  //represents path of the parent directory
-    private Path filePath; //Path represent the file itself.
-    private Path logFilePath; //Path representation for the log file
+    // properties for this class
+    // read in the file and parse it
+    private String directoryName;   // i.e. 'data', 'src/fileIo', etc
+    private String fileName;        // i.e. 'day18.txt', 'jolts.txt', etc
+    private String logFileName;     // will hold logging info
+    private Path directoryPath;     // Path representation for 'data', 'src/fileIo', etc
+    private Path filePath;          // Path representation for 'data/day18.txt', 'src/fileIo/jolts.txt', etc
+    private List<String> fileLines; // A list to iterate through with each line of the file
+    private List<String> logLines;  // A String list to hold all of the logging messages
+    private Path logFilePath;       // Path representation of the logging file
 
-    private List<String> fileLines; //Holding spot for the content of the data file.
-    private List<String> LogFileLines; //hold spot for contents of the logfile
 
-    //Contructors
-    public FileReader(String directoryName, String fileName, String logFileName){
+    // Constructor
+    public FileReader(String directoryName, String fileName, String logFileName) throws IOException {
         this.directoryName = directoryName;
         this.fileName = fileName;
         this.logFileName = logFileName;
-        // Instantiation Path values.
-        this.directoryPath = Paths.get(directoryName); // Paths.get("data")
+        this.directoryPath = Paths.get(directoryName);
         this.filePath = Paths.get(directoryName, fileName);
         this.logFilePath = Paths.get(directoryName, logFileName);
 
-
-        //Check for files, if they dont exist then we will create them.
-        //Log file first the data file
-        //Log file
-        if(Files.notExists(this.logFilePath)){
+        // Create a logging file if it doesn't already exist
+        if (Files.notExists(this.logFilePath)) {
             try {
                 Files.createFile(this.logFilePath);
-            }catch (IOException e){
-                //store exception message in the log file
-                throw new IOException("Unable to create the log file(" + this.logFileName + ")");
+            } catch (IOException e) {
+                // if there is an issue creating the log file, let's just crash the whole party
+                //   because we want to be able to log all errors
+                throw new IOException("Unable to create the logfile (" + this.logFileName + ")!");
             }
-
         }
 
-        System.out.println(filePath);
-    }
+        // Check to see if the directory path even exists
+        //    create it, if it doesn't exist
+        if (Files.notExists(this.directoryPath)) {
+            try {
+                Files.createDirectories(this.directoryPath); // createDirectory (only creates the single directory) vs createDirectories (creates non-existent parent directories if necessary)
+            } catch (IOException e) {
+                // Add the error message to the log
+                // this.logLines.add(e.getMessage());
+                Files.write(this.logFilePath, Arrays.asList(e.getMessage()), StandardOpenOption.APPEND);
+                throw new IOException("Unable to create the data directory (" + this.directoryName + ")!");
+                // stop all execution so we can investigate what went wrong
+            }
+        }
 
+        // Check to see if the file path even exists
+        //    create it, if it doesn't exist
+        if (Files.notExists(this.filePath)) {
+            try {
+                Files.createFile(this.filePath);
+            } catch (IOException e) {
+                // Add the error message to the log
+                // this.logLines.add(e.getMessage());
+                Files.write(this.logFilePath, Arrays.asList(e.getMessage()), StandardOpenOption.APPEND);
+                throw new IOException("Unable to create the data file (" + this.fileName + ")!");
+                // stop all execution so we can investigate what went wrong
+            }
+        }
+
+        // Take a look at the actual filePath value as a string
+        System.out.println(this.filePath.toString());
+        this.fileLines = Files.readAllLines(this.filePath); // populate the fileLines String array
+    }
+//PSVM STARTS HERE.................
     public static void main(String[] args) throws IOException {
-        FileReader thisFileReader = new FileReader("data", "day18.txt", "day18.log");
+        FileReader adventDayEighteen = new FileReader("data", "day18.txt", "day18_log.txt");
+        FileReader jolts = new FileReader("src/fileIo", "jolts.txt", "jolts_log.txt");
 
+        adventDayEighteen.writeToLog("Initialized file reader");
+        jolts.writeToLog("Initialized file reader");
 
-        FileReader joltsReader = new FileReader("src/fileIo", "jolts.txt", "jolts.log");
+        System.out.println(adventDayEighteen.getFileLines().get(0));
+        System.out.println(jolts.getFileLines().get(0));
+
+        adventDayEighteen.writeToLog("Retrieved first line of all data file lines.");
+        jolts.writeToLog("Retrieved first line of all data file lines.");
     }
-    //Custom methods
+//PSVM ENDS....................
 
-    //Getters and settters
+    // Custom message to easily write to the log whenever we want to!
+    public void writeToLog(String message) throws IOException {
+        try {
+            Files.write(this.getLogFilePath(), Arrays.asList(message), StandardOpenOption.APPEND);
+        } catch(IOException e) {
+            // store the exception message in our log file
+            Files.write(this.logFilePath, Arrays.asList(e.getMessage()), StandardOpenOption.APPEND);
+            throw new IOException("Unable to write exception message to log file!");
+        }
+    }
+
+    // Getters & Setters
     public String getDirectoryName() {
         return directoryName;
     }
@@ -95,14 +144,6 @@ public class FileReader {
         this.filePath = filePath;
     }
 
-    public Path getLogFilePath() {
-        return logFilePath;
-    }
-
-    public void setLogFilePath(Path logFilePath) {
-        this.logFilePath = logFilePath;
-    }
-
     public List<String> getFileLines() {
         return fileLines;
     }
@@ -111,11 +152,19 @@ public class FileReader {
         this.fileLines = fileLines;
     }
 
-    public List<String> getLogFileLines() {
-        return LogFileLines;
+    public List<String> getLogLines() {
+        return logLines;
     }
 
-    public void setLogFileLines(List<String> logFileLines) {
-        LogFileLines = logFileLines;
+    public void setLogLines(List<String> logLines) {
+        this.logLines = logLines;
+    }
+
+    public Path getLogFilePath() {
+        return logFilePath;
+    }
+
+    public void setLogFilePath(Path logFilePath) {
+        this.logFilePath = logFilePath;
     }
 }
